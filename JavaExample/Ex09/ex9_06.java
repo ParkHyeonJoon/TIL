9675b7a18b1039db841d4cdc192d7507df6b3ce5
Hyeonjoon <kingpark02@naver.com>
2023-11-29 12:24:47 +0900

    clone() 얕은 복사, 깊은 복사 예제

---
+++ JavaExample/Ex09/ex9_06.java
@@ -0,0 +1,27 @@
+package JavaExample.Ex09;
+
+class Card {
+    String kind;
+    int number;
+
+    Card() {
+        this("Space", 1); // Card(String kind, int number)를 호출
+    }
+
+    Card(String kind, int number) {
+        this.kind = kind;
+        this.number = number;
+    }
+
+    public String toString() {
+        return "kind : " + kind + ", number : " + number; // Card인스턴스의 kind와 number를 문자열로 출력
+    }
+}
+public class CardToString2 {
+    public static void main(String[] args) {
+        Card c1 = new Card();
+        Card c2 = new Card("Heart", 10);
+        System.out.println(c1.toString());
+        System.out.println(c2.toString());
+    }
+}
